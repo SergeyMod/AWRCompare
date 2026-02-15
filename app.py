8fc305f112f945cb84638c48e7f21a5cd713c05e
segaey <selkiev@lanit.ru>
2026-02-16 00:52:35 +0500

    chenge host

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ if __name__ == '__main__':
     print("Откройте в браузере: http://127.0.0.1:5000")
     print("=" * 60)
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
